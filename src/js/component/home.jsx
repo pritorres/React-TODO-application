b5c2de925347af6a194e37c3d97cb4a1429212f1
priscila torres reyes <priscyla.torres16@hotmail.com>
2021-09-09 22:34:31 +0000

    second

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [valorInput, setValorInput] = useState("");
+	const [tareas, setTareas] = useState([]);
+	const myInput = e => {
+		setValorInput(e.target.value);
+	};
+	const pessEnter = e => {
+		if (e.key === "Enter") {
+			setTareas([...tareas, valorInput]);
+			setValorInput("");
+		}
+	};
+	const myfunction = valortarea => {
+		return (
+			<div className="inputDiv">
+				{valortarea}
+				<button>X</button>
+			</div>
+		);
+	};
 	return (
 		<div className="container col-10">
 			<h1>todos</h1>
 			<div className="card">
-				<input type="text" />
+				<input
+					placeholder="No tasks, add a task"
+					value={valorInput}
+					type="text"
+					onChange={e => {
+						myInput(e);
+					}}
+					onKeyPress={e => {
+						pessEnter(e);
+					}}
+				/>
+				{tareas.map(myfunction)}
 			</div>
 		</div>
 	);
